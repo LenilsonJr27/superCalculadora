b5bc9695b978526c38e186372a97b83984bb0733
Lenilson Junior <lenilsonjrpro@gmail.com>
2024-11-04 16:18:47 -0300

    Projeto feito em grupo, numa aula para treinar GIT

---
+++ Models/Multiplicacao.cs
@@ -0,0 +1,10 @@
+﻿namespace SuperCalculadora.Modelos
+{
+    public class Multiplicacao : IOperacao
+    {
+        public double Calcular(double numero1, double numero2)
+        {
+            return numero1 * numero2;
+        }
+    }
+}
---
+++ Models/Soma.cs
@@ -0,0 +1,10 @@
+﻿namespace SuperCalculadora.Modelos
+{
+    public class Soma : IOperacao
+    {
+        public double Calcular(double numero1, double numero2)
+        {
+            return numero1 + numero2;
+        }
+    }
+}
